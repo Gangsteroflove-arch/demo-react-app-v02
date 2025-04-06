790fbf09e0f9711af0039e896b548d10beb0155e
gansteroflove-arch <reesie1998@gmail.com>
2025-04-06 04:39:26 -0400

    update react demo app 2

--- react-app-v02/src/components/Home.js
+++ react-app-v02/src/components/Home.js
@@ -3,10 +3,15 @@ import {Link} from "react-router-dom";
 function Home() {
     return (
       <>
-      <p>task on hand v1.0.4</p>
-      <p></p>
+        <p>task on hand v1.0.4</p>
+        <p></p>
+        <div>
+            <input type="text" maxlength={25} placeholder="* add item"/>
+            {" "} <button>add</button> {" "} <a>clear</a>
+            <p></p>
+        </div>
 
-      <Link to="/contactus">Contact Us</Link>
+         <Link to="/contactus">Contact Us</Link>
       </>
     );
   }
