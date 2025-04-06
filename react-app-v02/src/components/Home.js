cc911dd5718abf893442fd15237b143bd3fb0bf8
gansteroflove-arch <reesie1998@gmail.com>
2025-04-05 22:29:09 -0400

    update react demo app 2

--- react-app-v02/src/components/Home.js
+++ react-app-v02/src/components/Home.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Home() {
     return (
       <>
       <p>task on hand v1.0.4</p>
+      <p></p>
+
+      <Link to="/Contact Us"></Link>
       </>
     );
   }
