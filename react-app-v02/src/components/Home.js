b2a9322419aa052ff1fcd09c52477f558cc9fbe6
gansteroflove-arch <reesie1998@gmail.com>
2025-04-06 06:20:05 -0400

    update react demo app 2

--- react-app-v02/src/components/Home.js
+++ react-app-v02/src/components/Home.js
@@ -1,17 +1,28 @@
-import {Link} from "react-router-dom";
-//import Header form "./components/Header"
 function Home() {
+
     return (
       <>
-        <p>task on hand v1.0.4</p>
+        <h1>task on hand</h1>
         <p></p>
         <div>
-            <input type="text" maxlength={25} placeholder="* add item"/>
+            <input type="text" maxLength={25} placeholder="* add item"/>
             {" "} <button>add</button> {" "} <a>clear</a>
             <p></p>
         </div>
+        <p></p>
+        <a>all</a> {" | "}
+        <a>complete</a> {" | "}
+        <a>in-complete</a> {" | "}
+        <p></p>
+        <div>
+          <span>[] item A -x </span><br/>
+          <span>[] item B -x </span><br/>
+          <span>[] item C -x </span><br/>
+          <span>[x] item D -x </span><br/>
+          <span>[x] item E -x </span><br/>
+        </div>
 
-         <Link to="/contactus">Contact Us</Link>
+         
          <p></p>
          <p>footer 2025</p>
       </>
--- react-app-v02/src/components/contactus.js
+++ react-app-v02/src/components/contactus.js
@@ -1,18 +1,24 @@
  function ContactUs() {
+
     return (
         <>
-            <p>Contact Us</p>
+        <div>
+            <h1>Contact Us</h1>
             <form>
                 <div>
-                    <span>first name: </span> <input type="text" maxlength={25} placeholder="* first name"/>
+                    <span>first name: </span> <input type="text" maxLength={25} placeholder="* first name"/>
                     <br/>
-                    <span>last name: </span> <input type="text" maxlength={25} placeholder="* last name"/>
+                    <span>last name: </span> <input type="text" maxLength={25} placeholder=" last name"/>
                     <br/>
-                    <span>email:</span> <input type="text" maxlength={100} placeholder="* email"/>
+                    <span>email:</span> <input type="text" maxLength={100} placeholder="* email"/>
+                    <br/>
+                    <span>comment:</span> 
+                    <br/><textarea rows={10} cols={45} maxLength={100} placeholder="* comment"></textarea>
                     <p></p>
-                    <button>submit</button> {" "} <a href="#">clear</a>
+                    <button>submit</button> {" "} <a>clear</a>
                 </div>
             </form>
+         </div>
          </>
      );
 }
